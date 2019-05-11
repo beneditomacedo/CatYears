40d8e73830acc288da6d15c168268d8c0f2a6b9d
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2019-05-11 00:29:55 -0300

    some comments, clear the text in input field

--- CatYears/ViewController.swift
+++ CatYears/ViewController.swift
@@ -14,16 +14,27 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var yearsCat: UILabel!
     
     @IBAction func getAge(_ sender: Any) {
+        
+        // to dismiss the keyboard
+        //
         yearsHuman.resignFirstResponder()
+        
         if let age = yearsHuman.text {
             if let ageInt = Int(age) {
                 yearsCat.text = "Your cat is " + String (ageInt * 7) + " in cat years"
             }
         }
+        
+        // to clear the text in input field
+        //
+        yearsHuman.text = ""
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // to dismiss the keyboard
+        //
         self.yearsHuman.delegate = self
     }
 
